971cf612dfcf73f2b513286a9dfe92201d97e171
vignesh-zt132 <vignesh.thillai@zohocorp.com>
2019-07-19 12:51:53 +0530

    new method added to plugin

--- src/android/TestPluginForCordovaAndroid.java
+++ src/android/TestPluginForCordovaAndroid.java
@@ -1,5 +1,14 @@
 package com.test.plugin.android;
 
+import android.app.Activity;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.zoho.deskportalsdk.DeskConfig;
+import com.zoho.deskportalsdk.ZohoDeskPortalSDK;
+
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
 
@@ -12,11 +21,22 @@ import org.json.JSONObject;
  */
 public class TestPluginForCordovaAndroid extends CordovaPlugin {
 
+    private ZohoDeskPortalSDK deskPortalSDK;
+    private Activity activity;
+
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
         if (action.equals("coolMethod")) {
             String message = args.getString(0);
             this.coolMethod(message, callbackContext);
+            return true;
+        }
+        else if (action.equals("initialize")) {
+            this.initialize(args, callbackContext);
+            return true;
+        }
+        else if (action.equals("startDeskHelpCenter")) {
+            this.startDeskHelpCenter(callbackContext);
             return true;
         }
         return false;
@@ -28,5 +48,27 @@ public class TestPluginForCordovaAndroid extends CordovaPlugin {
         } else {
             callbackContext.error("Expected one non-empty string argument.");
         }
+    }
+
+    private void initialize(JSONArray array, CallbackContext callbackContext) {
+        activity = cordova.getActivity();
+        deskPortalSDK = ZohoDeskPortalSDK.getInstance(cordova.getActivity().getApplication());
+        DeskConfig config = new DeskConfig.Builder().build();
+        try {
+            JsonParser parser = new JsonParser();
+            JsonElement jsonElement = parser.parse(array.getString(0));
+            JsonArray args = jsonElement.getAsJsonArray();
+            long orgId = args.get(0).getAsLong();
+            String appId = args.get(1).getAsString();
+            deskPortalSDK.initDesk(orgId, appId, ZohoDeskPortalSDK.DataCenter.US, config);
+        } catch (Exception e) {
+            if(callbackContext != null) {
+                callbackContext.error("Error while initialize");
+            }
+        }
+    }
+
+    private void startDeskHelpCenter(CallbackContext callbackContext) {
+        deskPortalSDK.startDeskHomeScreen(activity);
     }
 }
--- www/TestPluginForCordovaAndroid.js
+++ www/TestPluginForCordovaAndroid.js
@@ -3,3 +3,11 @@ var exec = require('cordova/exec');
 exports.coolMethod = function (arg0, success, error) {
     exec(success, error, 'TestPluginForCordovaAndroid', 'coolMethod', [arg0]);
 };
+
+exports.initialize = function (arg0, success, error) {
+    exec(success, error, 'TestPlugin', 'initialize', [arg0]);
+};
+
+exports.startDeskHelpCenter = function (arg0, success, error) {
+    exec(success, error, 'TestPlugin', 'startDeskHelpCenter', [arg0]);
+};
