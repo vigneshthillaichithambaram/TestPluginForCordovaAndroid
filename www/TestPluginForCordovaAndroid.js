dd30657fb61ae0d3dc29fdaf5ccac55c7dfb1fca
vignesh-zt132 <vignesh.thillai@zohocorp.com>
2019-07-22 15:05:58 +0530

    js changes

--- www/TestPluginForCordovaAndroid.js
+++ www/TestPluginForCordovaAndroid.js
@@ -5,9 +5,9 @@ exports.coolMethod = function (arg0, success, error) {
 };
 
 exports.initialize = function (arg0, success, error) {
-    exec(success, error, 'TestPlugin', 'initialize', [arg0]);
+    exec(success, error, 'TestPluginForCordovaAndroid', 'initialize', [arg0]);
 };
 
 exports.startDeskHelpCenter = function (arg0, success, error) {
-    exec(success, error, 'TestPlugin', 'startDeskHelpCenter', [arg0]);
+    exec(success, error, 'TestPluginForCordovaAndroid', 'startDeskHelpCenter', [arg0]);
 };
